306022ebf39d27971a5f297ac7f6cf88eb44e06f
minamii <49612620+minamii37@users.noreply.github.com>
2022-07-17 13:44:04 +0900

    コミット漏れ？

--- ClinicScheduler.Domain/Models/ValueObject/ReservationValueObject.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace ClinicScheduler.Domain.Models.ValueObjects
-{
-    public record ReservationValueObject(
-        string ReservationId, string DoctorId, DateTime TargetDateTime, string PatintId, DateTime ReservationDatetime);
-}
-
